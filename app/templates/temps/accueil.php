f296b74513d76a264324a57629fcb876dedcb434
intdevweb <jp_informatique@hotmail.fr>
2015-08-25 13:47:25 +0200

    add accueil model templates, route, layout, style, home controller

---
+++ app/templates/temps/accueil.php
@@ -0,0 +1,18 @@
+<?php $this->layout('layout', ['title' => ' Bdloc']) ?>
+
+<?php $this->start('main_content') ?>
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:300' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="<?=$this->assetUrl('css/style.css') ?>">
+	<title>BDLOC</title>
+</head>
+<body>
+
+
+<h2> ACCUEIL</h2>
+</body>
+<?php $this->stop('main_content') ?>
+</html>
