d494f9b002825cf33024a2c7aad6c8e44d48ccaa
intdevweb <jp_informatique@hotmail.fr>
2015-08-26 14:56:41 +0200

    insert if pour form

--- app/Controller/UserController.php
+++ app/Controller/UserController.php
@@ -1,12 +1,12 @@
 <?php
-	namespace Controller;
+namespace Controller;
 
-	use \W\Controller\Controller;
-	use \Manager\UserManager;
-	use \W\security\authentificationManager;
+use \W\Controller\Controller;
+use \Manager\UserManager;
+use \W\security\authentificationManager;
 
-	class UserController extends Controller
-	{
+class UserController extends Controller
+{
 
 		// public function logout()
 		// {
@@ -15,66 +15,68 @@
 		// 	$this->redirectionToRoute('admin');
 		// }
 
-		public function login()
-		 {
-		 	$am = new authentificationManager();
-			$error="";
-		 	$username="";
+	public function login()
+	{
+		$am = new authentificationManager();
+		$error="";
+		$username="";
 		 	//$date = [];
 
 		 	// traitement du formulaire
-		 	if(!empty($_POST)){
-		 		$username = $_POST['username'];
-				$password = $_POST['password'];
+		if(!empty($_POST)){
+			$username = $_POST['username'];
+			$password = $_POST['password'];
 
-		 		$result = $am->isValidLoginInfo($username, $password);
+			$result = $am->isValidLoginInfo($username, $password);
 
 			// si valide : connexion
-		 		if($result > 0){
-		 			$userId = $result;
+			if($result > 0){
+				$userId = $result;
 
 					// récuper l'utilisateur
-					$UserManager = new \Manager\UserManager();
-	 			$user = $UserManager->find($userId);
+				$UserManager = new \Manager\UserManager();
+				$user = $UserManager->find($userId);
 
 					// connexion l'user
-		 			$am->loginUserIn($user);
+				$am->loginUserIn($user);
 
 		// 			// redirection
 
 		// 			$this->redirectionToRoute('show_all_terms');
 
-					
-		 		}else{
-		 			$error = "Mauvais identifiants !";
-				}
+
+			}else{
+				$error = "Mauvais identifiants !";
 			}
+		}
 
 		// 	$date['error'] = $error;
 		// 	$data['username'] = $username;
-		 $this->show('temps/connexion');
-		 }
+		$this->show('temps/connexion');
+	}
 
-		public function register()
-		{
+	public function register()
+	{
 			// $this->allowTo('admin');
-			$user = $this->getUser();
-			
+		$user = $this->getUser();
 
-			$UserManager = new UserManager();
-			$error = "";
-			$username = "";
-			$email = "";
-			$password = "";
-			$password_confirm = "";
-			$hashedPassword = "";
-			$nom = "";
-			$prenom = "";
-			$code_postal = "";
-			$adress = "";
-			$tel = "";
 
-			
+		$UserManager = new UserManager();
+		$error = [];
+		$username = "";
+		$email = "";
+		$password = "";
+		$password_confirm = "";
+		$hashedPassword = "";
+		$nom = "";
+		$prenom = "";
+		$code_postal = "";
+		$adress = "";
+		$tel = "";
+		$code_postalRegexp = "/^[0-9]{5}$/";
+		$telRegexp = "/^[0-9]{10}$/";
+
+
 			// formulaire d'inscription -------------------------------------
 			// if(!empty($_POST)){
 			// 	foreach ($$_POST as $k => $v) {
@@ -82,79 +84,118 @@
 			// 		$$k = trim(strip_tags($v));
 			// } ------------------------------------------------------------
 
-			if(!empty($_POST)){
-				$username 			=trim(strip_tags($_POST['username']));
-				$email 				=trim(strip_tags($_POST['email']));
-				$password 			=trim(strip_tags($_POST['password']));
-				$password_confirm 	=trim(strip_tags($_POST['password_confirm']));
-				$nom 				=trim(strip_tags($_POST['nom']));
-				$prenom 			=trim(strip_tags($_POST['prenom']));
-				$code_postal 		=trim(strip_tags($_POST['code_postal']));
-				$adress 			=trim(strip_tags($_POST['adress']));
-				$tel 				=trim(strip_tags($_POST['tel']));
-				
-				/* validation */
+		if(!empty($_POST)){
+			$username 			=trim(strip_tags($_POST['username']));
+			$email 				=trim(strip_tags($_POST['email']));
+			$password 			=trim(strip_tags($_POST['password']));
+			$password_confirm 	=trim(strip_tags($_POST['password_confirm']));
+			$nom 				=trim(strip_tags($_POST['nom']));
+			$prenom 			=trim(strip_tags($_POST['prenom']));
+			$adress 			=trim(strip_tags($_POST['adress']));
+			$code_postal 		=trim(strip_tags($_POST['code_postal']));
+			$tel 				=trim(strip_tags($_POST['tel']));
+
+			/* validation */
+			if(empty($username)){
+				$error['username'] = "Veuillez saisir votre username !";
+
+			}
 					// username assez long
-					if(strlen($username) < 4){
-						$error = "pseudo trop court !";
-					}
+			if(strlen($username) < 4){
+				$error['username'] = "pseudo trop court !";
+
+			}/*else{
+				$sql = "SELECT username FROM users WHERE username = :username";
+				$sth = $dbh->prepare($sql);
+				$sth -> execute(array(":username"=> $username));
+				$foundUsername = $sth->fetchColumn();
+				if ($foundUsername){
+					$error= "ce username existe déjà";
+				}
+			}*/
 
 					// email
-					if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
-						$error = "email non valide !";
-					}
-
-					// email present
+			if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+				$error['email'] = "email non valide !";
+			}
+			elseif(strlen($email)> 250){
+				$error['email'] = "email trop long non valide !";
+			}					
 
 					// mot de passe correspondent
-					if($password != $password_confirm){
-						$error = "Les mots de passe ne correspondent pas !";
-					}
-				/* fin de la validation */
+			if($password != $password_confirm){
+				$error['password'] = "Les mots de passe ne correspondent pas !";
+			}elseif(strlen($password) < 6){
+				$error['password'] = "veuillez saisir un mot de passe d'au moins 7 caracteres !";
+			}						
+		
+		if(empty($nom)){
+			$error['nom'] = "Veuillez saisir votre nom !";
+		}
+		if(empty($prenom)){
+			$error['prenom'] = "Veuillez saisir votre prenom !";
+		}
+		if(empty($adress)){
+			$error['adress'] = "Veuillez saisir votre adress !";
+		}
+		if(empty($code_postal)){
+			$error['code_postal'] = "Veuillez saisir votre code postal!";
+		}elseif(!preg_match($code_postalRegexp, $code_postal)){
+			$error['code_postal'] = "votre code postal doit correspondre à 5 chiffres";
+		}
+		if(empty($tel)){
+			$error['tel'] = "Veuillez saisir votre tel!";
+		}elseif(!preg_match($telRegexp, $tel)){
+			$error['tel'] = "votre numero de tel doit correspondre à 10 chiffres";
+		}
+
+		/* fin de la validation */
 
 					// si valide...
-						if(empty($error)){
+		if(empty($error)){
 
-						}
+		//}
 						// hacher le mot de passe 
-						$hashedPassword  = password_hash($password, PASSWORD_DEFAULT);
-						
-						$newUser = [
-									"username" 		=> $username,
-									"email" 		=> $email,
-									"password" 		=> $hashedPassword,
-									"nom" 			=> $nom,
-									"prenom" 		=> $prenom,
-									"code_postal" 	=> $code_postal,
-									"adress" 		=> $adress,
-									"tel" 			=> $tel,
+		$hashedPassword  = password_hash($password, PASSWORD_DEFAULT);
+
+		$newUser = [
+		"username" 		=> $username,
+		"email" 		=> $email,
+		"password" 		=> $hashedPassword,
+		"nom" 			=> $nom,
+		"prenom" 		=> $prenom,
+		"code_postal" 	=> $code_postal,
+		"adress" 		=> $adress,
+		"tel" 			=> $tel,
 									// "role" 			=> "admin",
-									"date_created"	=> date("Y-m-d H:i:s"),
-									"date_modified" 	=> date("Y-m-d H:i:s"),
-						];
+		"date_created"	=> date("Y-m-d H:i:s"),
+		"date_modified" 	=> date("Y-m-d H:i:s"),
+		];
 						// debug($_POST);
 						// die();
 						//inseérrer en base
-						$UserManager->insert($newUser);
+		$UserManager->insert($newUser);
+	}
 
 						//afficher bravo ou rediriger ou faire quelque 
 
 					//si invalide..
 					   //envoyer les erreurs et les données soumises à la vue 
-			}
-			// $dataToPassToTheView = [
-			// 	"error" 		=> $error,
-			// 	"username" 		=> $username,
-			// 	"email" 		=> $email,
-			// 	"password" 		=> $hashedPassword,
-			// 	"nom" 			=> $nom,
-			// 	"prenom" 		=> $prenom,
-			// 	"code_postal" 	=> $code_postal,
-			// 	"adress" 		=> $adress,
-			// 	"tel" 			=> $tel,
-			// ];	
-			$this->show('temps/abonnement');
-			// $this->RedirectToRoute('abonnement');
-		}
-
 	}
+	$dataToPassToTheView = [
+	"username" 		=> $username,
+	"email" 		=> $email,
+	"password" 		=> $password,
+	"nom" 			=> $nom,
+	"prenom" 		=> $prenom,
+	"code_postal" 	=> $code_postal,
+	"adress" 		=> $adress,
+	"tel" 			=> $tel,
+	"errors" 		=> $error,
+	];	
+	$this->show('temps/abonnement', $dataToPassToTheView);
+			// $this->RedirectToRoute('abonnement');
+}
+
+
+}
--- app/templates/temps/abonnement.php
+++ app/templates/temps/abonnement.php
@@ -6,39 +6,46 @@
 
 		<p> Veuillez renseigner les champs suivants </p>
 		<div class="forme">	
-			<form method="POST" action="">
+			<form method="POST" novalidate action="">
 				<label>Username</label>
-				<input type="text" id='nam' name='username' value><br />
+				<input type="text" id='username' name='username'  ><div><?php if (!empty($errors['username'])){echo $errors['username'];}?></div><br />
+
 
 
 				<label>Email</label>
-				<input type="email" id='nam' name='email' value><br />
+				<input type="email" id='email' name='email' required><div><?php if (!empty($errors['email'])){echo $errors['email'];}?></div><br />
 
 				<label>Mot de passe </label>
-				<input type="password" id='password' name='password' value><br />
+				<input type="password" id='password' name='password' required ><div><?php if (!empty($errors['password'])){echo $errors['password'];}?></div><br/>
 
 				<label>Confirm mot de passe </label>
-				<input type="password" id='password' name='password_confirm' value><br /><br />
+				<input type="password" id='password_confirm' name='password_confirm' required ><br /><br />
 
 				<label>Nom</label>
-				<input type="text" id='nom' name='nom' value><br />
+				<input type="text" id='nom' name='nom' required ><div><?php if (!empty($errors['nom'])){echo $errors['nom'];}?></div><br />
 
 				<label>Prenom</label>
-				<input type="text" id='prenom' name='prenom' value><br />
+				<input type="text" id='prenom' name='prenom' required ><div><?php if (!empty($errors['prenom'])){echo $errors['prenom'];}?></div><br />
 
 				<label>Code_Postal</label>
-				<input type="text" id='code_postal' name='code_postal' value><br />
+				<input type="text" id='code_postal' name='code_postal' required ><div><?php if (!empty($errors['code_postal'])){echo $errors['code_postal'];}?></div><br />
 
 				<label>Adress</label>
-				<input type="text" id='adress' name='adress' value><br />
+				<input type="text" id='adress' name='adress' required ><div><?php if (!empty($errors['adress'])){echo $errors['adress'];}?></div><br />
 
 				<label>Tel</label>
-				<input type="text" id='tel' name='tel' value><br />
+				<input type="text" id='tel' name='tel' required ><div><?php if (!empty($errors['tel'])){echo $errors['tel'];}?></div><br />
 
 				<input type="submit" name="Valider">
 				<!--input type="reset" name="reset"-->
+				<div class="identification">
+				<a href="<?= $this->url('connexion'); ?>" title="connexion">Connexion !</a>
+			
 			</form>
+			
 		</div>
+		
+
 
 	<?php $this->stop('main_content') ?>
 
