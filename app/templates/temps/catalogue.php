feb3f488c2cfadadcd898eed8cca0752ef29bd13
nadiadev <creation.nadia@gmail.com>
2015-08-26 16:27:20 +0200

    add view catalogue

---
+++ app/templates/temps/catalogue.php
@@ -0,0 +1,26 @@
+<?php $this->layout('layout', ['title' => ' Catalogue BDLOC']) ?>
+
+<?php $this->start('main_content') ?>
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:300' rel='stylesheet' type='text/css'>
+	
+	<title>BDLOC</title>
+</head>
+<body>
+
+<h2> ACCUEIL</h2>
+
+
+
+
+
+
+
+
+
+</body>
+<?php $this->stop('main_content') ?>
+</html>
